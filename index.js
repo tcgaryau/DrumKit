693c4a68e9704464c1b316403ed937155cd398b1
Gary Au <tcgaryau@gmail.com>
2021-12-27 16:01:02 -0800

    added an animation when the button is pressed

--- index.js
+++ index.js
@@ -4,11 +4,13 @@ for (i = 0; i < drum.length; i++) {
   drum[i].addEventListener("click", function () {
     let buttonInnerHTML = this.innerHTML;
     makeSound(buttonInnerHTML);
+    buttonAnimation(buttonInnerHTML);
   });
 }
 
 document.addEventListener("keydown", function (event) {
   makeSound(event.key);
+  buttonAnimation(event.key);
 });
 
 function makeSound(key) {
@@ -44,4 +46,12 @@ function makeSound(key) {
     default:
       break;
   }
+}
+
+function buttonAnimation(currentKey) {
+  let activeButton = document.querySelector("." + currentKey);
+  activeButton.classList.add("pressed");
+  setTimeout(function () {
+    activeButton.classList.remove("pressed");
+  }, 100);
 }
